414e997574e91f8ac05fd8668c09b44a822a6d1c
Depleted <odepleted@gmail.com>
2023-12-11 22:46:57 +1300

    Removed old commented code

--- src/pages/Home/sections/ProjectsSection.js
+++ src/pages/Home/sections/ProjectsSection.js
@@ -23,25 +23,6 @@ const ProjectsSection = forwardRef((_, ref) => {
               <ProjectPane projectRoute={projectRoute} projectData={projectData} />
             </div>
           ))}
-
-          {/* {range(0, 6).map((i) => {
-            return (
-              <div data-aos="fade-right" data-aos-delay={i * 100} key={i}>
-                <ProjectPane
-                  projectData={{
-                    thumbnail_url:
-                      "https://media.istockphoto.com/id/1407983911/photo/forex-diagrams-and-stock-market-rising-lines-with-numbers.jpg?s=612x612&w=0&k=20&c=zas1h6LR6v2iCvE7SWnVoZ_s7ZSiboN45UK0d5oMWac=",
-                    route: "/projects/test",
-                    title: "Statalytics",
-                    description: "Bedwars stats done right",
-                    github: "https://statalytics.net/github",
-                    website: "https://statalytics.net/",
-                    tags: ["Full Stack", "Statistics"],
-                  }}
-                />
-              </div>
-            );
-          })} */}
         </div>
       </div>
     </div>
