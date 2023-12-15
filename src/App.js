36b937787cdb4127a6da8b9d839016c34222e2a1
Depleted <odepleted@gmail.com>
2023-12-16 11:54:43 +1300

    idk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
