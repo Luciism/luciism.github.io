2ef20a9432de9d24b2df8966ff50134578e5a07f
Depleted <odepleted@gmail.com>
2023-12-16 13:18:37 +1300

    Changed back to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
