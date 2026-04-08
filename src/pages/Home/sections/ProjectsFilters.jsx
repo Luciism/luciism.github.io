e6b12832d4f11504451ec095c4d2530d0cefa084
Luciism <lucism.dev@gmail.com>
2026-04-09 07:19:55 +1200

    fix: improve scaling of filters panel

--- src/pages/Home/sections/ProjectsFilters.jsx
+++ src/pages/Home/sections/ProjectsFilters.jsx
@@ -120,163 +120,159 @@ const ProjectsFilters = ({
 
   return (
     <form id="filters" className="filters" data-aos="fade-up">
-      <div className="fieldset-container">
-        <fieldset>
-          <legend className="title-small">
-            <Icon
-              icon="lucide:filter"
-              width={16}
-              height={16}
-              className="icon"
-            />
-            <span>Project Type</span>
-          </legend>
-          <div className="field-options">
-            {appData.availableProjectProperties.map((properties, i) => {
-              const handleProjectTypeChange = (e) => {
-                if (e.target.checked) {
-                  setSelectedProjectProperties(
-                    new Set([...selectedProjectProperties, properties.id])
-                  );
-                } else {
-                  setSelectedProjectProperties(
-                    new Set(
-                      [...selectedProjectProperties].filter(
-                        (id) => id !== properties.id
-                      )
+      <fieldset>
+        <legend className="title-small">
+          <Icon
+            icon="lucide:filter"
+            width={16}
+            height={16}
+            className="icon"
+          />
+          <span>Project Type</span>
+        </legend>
+        <div className="field-options">
+          {appData.availableProjectProperties.map((properties, i) => {
+            const handleProjectTypeChange = (e) => {
+              if (e.target.checked) {
+                setSelectedProjectProperties(
+                  new Set([...selectedProjectProperties, properties.id])
+                );
+              } else {
+                setSelectedProjectProperties(
+                  new Set(
+                    [...selectedProjectProperties].filter(
+                      (id) => id !== properties.id
                     )
-                  );
-                }
-              };
+                  )
+                );
+              }
+            };
 
-              return (
-                <label className="body-small check" key={i}>
-                  <input
-                    className="input-checkbox"
-                    type="checkbox"
-                    name="project-type"
-                    value="web-development"
-                    onChange={handleProjectTypeChange}
-                    checked={selectedProjectProperties.has(properties.id)}
-                  />{" "}
-                  {properties.name}
-                </label>
-              );
-            })}
-          </div>
-        </fieldset>
+            return (
+              <label className="body-small check" key={i}>
+                <input
+                  className="input-checkbox"
+                  type="checkbox"
+                  name="project-type"
+                  value="web-development"
+                  onChange={handleProjectTypeChange}
+                  checked={selectedProjectProperties.has(properties.id)}
+                />{" "}
+                {properties.name}
+              </label>
+            );
+          })}
+        </div>
+      </fieldset>
 
-        <fieldset>
-          <legend className="title-small">
-            <Icon icon="ri:code-line" width={18} height={18} className="icon" />
-            <span>Technologies</span>
-          </legend>
-          <div className="field-options technologies-chips">
-            {techFilterChipsData.map((chip, i) => {
-              const handleTechChipCheck = (e) => {
-                if (e.target.checked) {
-                  setSelectedTechFilters(
-                    new Set([...selectedTechFilters, chip.id])
-                  );
-                } else {
-                  setSelectedTechFilters(
-                    new Set(
-                      [...selectedTechFilters].filter((id) => id !== chip.id)
-                    )
-                  );
-                }
-              };
+      <fieldset>
+        <legend className="title-small">
+          <Icon icon="ri:code-line" width={18} height={18} className="icon" />
+          <span>Technologies</span>
+        </legend>
+        <div className="field-options technologies-chips">
+          {techFilterChipsData.map((chip, i) => {
+            const handleTechChipCheck = (e) => {
+              if (e.target.checked) {
+                setSelectedTechFilters(
+                  new Set([...selectedTechFilters, chip.id])
+                );
+              } else {
+                setSelectedTechFilters(
+                  new Set(
+                    [...selectedTechFilters].filter((id) => id !== chip.id)
+                  )
+                );
+              }
+            };
 
-              return (
-                <label
-                  className="body-small technology-chip"
-                  style={{ "--chip-custom-color": chip.rgb }}
-                  key={i}
-                >
-                  <input
-                    className="technology-chip"
-                    type="checkbox"
-                    name="technology"
-                    value={chip.id}
-                    onChange={handleTechChipCheck}
-                    checked={selectedTechFilters.has(chip.id)}
-                  />{" "}
-                  <img src={chip.icon} alt={chip.name} />
-                  {chip.name}
-                </label>
-              );
-            })}
-          </div>
-        </fieldset>
-      </div>
+            return (
+              <label
+                className="body-small technology-chip"
+                style={{ "--chip-custom-color": chip.rgb }}
+                key={i}
+              >
+                <input
+                  className="technology-chip"
+                  type="checkbox"
+                  name="technology"
+                  value={chip.id}
+                  onChange={handleTechChipCheck}
+                  checked={selectedTechFilters.has(chip.id)}
+                />{" "}
+                <img src={chip.icon} alt={chip.name} />
+                {chip.name}
+              </label>
+            );
+          })}
+        </div>
+      </fieldset>
 
-      <div className="fieldset-container">
-        <fieldset>
-          <legend className="title-small">
+      <fieldset>
+        <legend className="title-small">
+          <Icon
+            icon="tdesign:order-ascending"
+            width={16}
+            height={16}
+            className="icon"
+          />
+          <span>Sort By</span>
+        </legend>
+
+        <label className="body-small" htmlFor="sort-by">
+          Set the project ordering
+        </label>
+        <div className="field-options">
+          <select
+            className="input-select"
+            id="sort-by"
+            name="sort-by"
+            defaultValue={sortingOption}
+            onChange={(e) => setSortingOption(e.target.value)}
+          >
+            <option value="relevance">Relevance</option>
+            <option value="newest-first">Newest First</option>
+            <option value="oldest-first">Oldest First</option>
+            <option value="alphabetical">A-Z</option>
+            <option value="rev-alphabetical">Z-A</option>
+          </select>
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <legend className="title-small">
+          <Icon
+            icon="tdesign:search"
+            width={16}
+            height={16}
+            className="icon"
+          />
+          <span>Results</span>
+        </legend>
+
+        <label className="body-small" htmlFor="sort-by">
+          Showing {filteredProjects.length} of {projects.length} projects
+        </label>
+        <div className="field-options">
+          <button
+            className="input-button"
+            type="reset"
+            onClick={() => {
+              setSortingOption("relevance");
+              setSelectedProjectProperties(new Set());
+              setSelectedTechFilters(new Set());
+            }}
+          >
             <Icon
-              icon="tdesign:order-ascending"
-              width={16}
-              height={16}
+              icon="charm:refresh"
+              width={14}
+              height={14}
               className="icon"
             />
-            <span>Sort By</span>
-          </legend>
-
-          <label className="body-small" htmlFor="sort-by">
-            Set the project ordering
-          </label>
-          <div className="field-options">
-            <select
-              className="input-select"
-              id="sort-by"
-              name="sort-by"
-              defaultValue={sortingOption}
-              onChange={(e) => setSortingOption(e.target.value)}
-            >
-              <option value="relevance">Relevance</option>
-              <option value="newest-first">Newest First</option>
-              <option value="oldest-first">Oldest First</option>
-              <option value="alphabetical">A-Z</option>
-              <option value="rev-alphabetical">Z-A</option>
-            </select>
-          </div>
-        </fieldset>
-
-        <fieldset>
-          <legend className="title-small">
-            <Icon
-              icon="tdesign:search"
-              width={16}
-              height={16}
-              className="icon"
-            />
-            <span>Results</span>
-          </legend>
-
-          <label className="body-small" htmlFor="sort-by">
-            Showing {filteredProjects.length} of {projects.length} projects
-          </label>
-          <div className="field-options">
-            <button
-              className="input-button"
-              type="reset"
-              onClick={() => {
-                setSortingOption("relevance");
-                setSelectedProjectProperties(new Set());
-                setSelectedTechFilters(new Set());
-              }}
-            >
-              <Icon
-                icon="charm:refresh"
-                width={14}
-                height={14}
-                className="icon"
-              />
-              Reset Filters
-            </button>
-          </div>
-        </fieldset>
-      </div>
+            Reset Filters
+          </button>
+        </div>
+      </fieldset>
     </form>
   );
 };
